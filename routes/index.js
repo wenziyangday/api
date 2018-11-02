8dba194758c0441c1e0bf49847bf17b8c3cb93a9
闻紫阳 <wenzy@orderhandler.com>
2018-11-02 13:57:53 +0800

    测试服务器上操作步骤

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-	res.render('index', {title: '加油你是最棒的！！！'});
+	res.render('index', {title: '还需要继续！！！'});
 });
 
 module.exports = router;
